96ba2d598d1559c75326d02ffcd9359ecdf68376
seth shoemaker <contact@sethshoemaker.com>
2024-05-04 22:21:58 -0400

    add content type to response functions

--- tekoi/framework/response.py
+++ tekoi/framework/response.py
@@ -4,52 +4,72 @@ import json as _json
 class Response(_startup.Response):
     pass
 
-def ok(body: bytes) -> Response:
+def ok(body: bytes, content_type: str = "text/plain") -> Response:
     res = Response()
     res.body = body
     res.content_length = len(body)
+    res.content_type = content_type
     res.status_code = 200
     return res
 
 def ok_json(payload: dict|list) -> Response:
-    return ok(body=_json.dumps(payload).encode())
+    return ok(
+        body=_json.dumps(payload).encode(),
+        content_type="application/json"
+    )
 
-def ok_created(body: bytes) -> Response:
+def ok_created(body: bytes, content_type: str = "text/plain") -> Response:
     res = Response()
     res.body = body
     res.content_length = len(body)
+    res.content_type = content_type
     res.status_code = 201
     return res
 
 def ok_created_json(payload: dict|list) -> Response:
-    return ok_created(body=_json.dumps(payload).encode())
+    return ok_created(
+        body=_json.dumps(payload).encode(),
+        content_type="application/json"
+    )
 
-def bad_request(body: bytes) -> Response:
+def bad_request(body: bytes, content_type: str = "text/plain") -> Response:
     res = Response()
     res.body = body
     res.content_length = len(body)
+    res.content_type = content_type
     res.status_code = 400
     return res
 
 def bad_request_json(payload: dict|list) -> Response:
-    return bad_request(_json.dumps(payload).encode())
+    return bad_request(
+        body=_json.dumps(payload).encode(),
+        content_type="application/json"
+    )
 
-def not_found(body: bytes) -> Response:
+def not_found(body: bytes, content_type: str = "text/plain") -> Response:
     res = Response()
     res.body = body
     res.content_length = len(body)
+    res.content_type = content_type
     res.status_code = 404
     return res
 
 def not_found_json(payload:dict) -> Response:
-    return not_found(_json.dumps(payload).encode())
+    return not_found(
+        body=_json.dumps(payload).encode(),
+        content_type="application/json"
+    )
 
-def internal_server_error(body: bytes) -> Response:
+def internal_server_error(body: bytes, content_type: str = "text/plain") -> Response:
     res = Response()
     res.body = body
     res.content_length = len(body)
+    res.content_type = content_type
     res.status_code = 500
     return res
 
 def internal_server_error_json(payload: dict|list) -> Response:
-    return internal_server_error(_json.dumps(payload).encode())
+    return internal_server_error(
+        body=_json.dumps(payload).encode(),
+        content_type="application/json"
+    )
